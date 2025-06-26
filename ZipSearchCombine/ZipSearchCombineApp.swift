c25f3a9537ca69e3188d3dc18545c4cd6c555463
仲優樹 <yu-ki216m@outlook.jp>
2025-06-26 16:12:52 +0900

    Initial Commit

---
+++ ZipSearchCombine/ZipSearchCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZipSearchCombineApp.swift
+//  ZipSearchCombine
+//
+//  Created by 仲優樹 on 2025/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ZipSearchCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
